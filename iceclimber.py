8a39d25caa3109edd266265640c83a7047e78590
Ritiek Malhotra <Ritiek@users.noreply.github.com>
2017-02-20 10:44:36 +0530

    create base

--- iceclimber.py
+++ iceclimber.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 
 import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((x,y))
+clock = pygame.time.Clock()
+
+done = False
+
+while not done:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
+		pygame.get.key_pressed()
+		if pressed[pygame.K_UP]:
+		if pressed[pygame.K_LEFT]:
+		if pressed[pygame.K_RIGHT]:
+	pygame.time.Clock(60)
+	pygame.display.flip()
