20efbde2714fcd46d1561c0ec2a5d981f800d811
Ritiek Malhotra <Ritiek@users.noreply.github.com>
2017-02-23 11:19:51 +0530

    add IceClimber menu

--- iceclimber.py
+++ iceclimber.py
@@ -3,8 +3,16 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((x,y))
-clock = pygame.time.Clock()
+screen = pygame.display.set_mode((770,620))
+pygame.display.set_caption('NES - Ice Climber')
+
+images, sounds = {}, {}
+
+images['menu'] = pygame.image.load('assets/sprites/menu.png').convert_alpha()
+sounds['menu'] = pygame.mixer.Sound('assets/audio/IceClimber.wav')
+
+screen.blit(images['menu'], (0,0))
+sounds['menu'].play(-1)
 
 done = False
 
@@ -12,9 +20,9 @@ while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
-		pygame.get.key_pressed()
-		if pressed[pygame.K_UP]:
-		if pressed[pygame.K_LEFT]:
-		if pressed[pygame.K_RIGHT]:
-	pygame.time.Clock(60)
+	#pressed = pygame.get.key_pressed()
+	#if pressed[pygame.K_UP]: pass
+	#if pressed[pygame.K_LEFT]: pass
+	#if pressed[pygame.K_RIGHT]: pass
+	pygame.time.Clock().tick(60)
 	pygame.display.flip()
