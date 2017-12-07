32e992141bbdec591b2fb96242215f67009f5dcd
ritiek <ritiekmalhotra123@gmail.com>
2017-12-07 14:29:25 +0530

    Consistent indentation

--- iceclimber.py
+++ iceclimber.py
@@ -55,39 +55,39 @@ player = sheet.clip(rectangle=(4, 22, 14, 23), scale=(27, 47))
 
 
 def main_menu(state):
-	print('main_menu(' + str(state) + ')')
-	if state:	# if state==True
+    print('main_menu(' + str(state) + ')')
+    if state:   # if state==True
         # load background image
-		screen.blit(images['main_menu'], (0,0))
+        screen.blit(images['main_menu'], (0,0))
         # loop main theme forever
-		sounds['main_menu'].play(-1)
-	else:
-		sounds['main_menu'].stop()
+        sounds['main_menu'].play(-1)
+    else:
+        sounds['main_menu'].stop()
 
 
 def render_start():
-	print('render_start()')
-	showing_level = sounds['show_level'].play()
-	for scroll_Y_axis in [n/2.0 for n in range(-2380, 0, 1)]:
-		screen.fill((0, 0, 0))
-		screen.blit(images['level'], (0, scroll_Y_axis))
-		pygame.display.update()
-	# sound is playing
-	while showing_level.get_busy():
-		pygame.time.wait(100)
+    print('render_start()')
+    showing_level = sounds['show_level'].play()
+    for scroll_Y_axis in [n/2.0 for n in range(-2380, 0, 1)]:
+        screen.fill((0, 0, 0))
+        screen.blit(images['level'], (0, scroll_Y_axis))
+        pygame.display.update()
+    # sound is playing
+    while showing_level.get_busy():
+        pygame.time.wait(100)
 
 
 def main_game():
-	print('main_game()')
-	screen.fill((0, 0, 0))
-	screen.blit(images['level'], (0, -1190))
-	screen.blit(player, (256, 415))
-	sounds['background'].play(-1)
-	pygame.display.update()
+    print('main_game()')
+    screen.fill((0, 0, 0))
+    screen.blit(images['level'], (0, -1190))
+    screen.blit(player, (256, 415))
+    sounds['background'].play(-1)
+    pygame.display.update()
 
 
 def jump():
-	sounds['jump'].play()
+    sounds['jump'].play()
 
 
 is_in_game = False
@@ -95,16 +95,16 @@ done = False
 
 main_menu(state=True)
 while not done:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			done = True
-		if event.type == KEYDOWN and event.key == K_RETURN and not is_in_game:
-			main_menu(state=False)
-			render_start()
-			main_game()
-			is_in_game = True
-		if event.type == KEYDOWN and event.key == K_x and is_in_game:
-			jump()
-	# run the window with max 60 fps
-	pygame.time.Clock().tick(60)
-	pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == KEYDOWN and event.key == K_RETURN and not is_in_game:
+            main_menu(state=False)
+            render_start()
+            main_game()
+            is_in_game = True
+        if event.type == KEYDOWN and event.key == K_x and is_in_game:
+            jump()
+    # run the window with max 60 fps
+    pygame.time.Clock().tick(60)
+    pygame.display.update()
