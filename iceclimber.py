c17ae3da461769834249e57eb8d105b41d6bacda
ritiek <ritiekmalhotra123@gmail.com>
2017-12-11 21:56:46 +0530

    Little climber walks now!

--- iceclimber.py
+++ iceclimber.py
@@ -11,6 +11,8 @@ pygame.init()
 screen = pygame.display.set_mode((512,480))
 # set window title
 pygame.display.set_caption('NES - Ice Climber')
+PLAYER_POS = (256, 415)
+PLAYER_SPEED = 4
 
 
 class LoadAssets:
@@ -38,15 +40,23 @@ class SpriteSheet:
     def __init__(self, path):
         self.sheet = pygame.image.load(path).convert_alpha()
 
-    def clip(self, rectangle, scale):
+    def clip(self, rectangle, scale=None):
         self.sheet.set_clip(pygame.Rect(rectangle))
         sprite = self.sheet.subsurface(self.sheet.get_clip())
-        scaled_sprite = pygame.transform.scale(sprite, scale)
-        return scaled_sprite
+        if scale:
+            sprite = pygame.transform.scale(sprite, scale)
+        return sprite
 
     def make_clips(self):
         sprites = {}
-        sprites['player'] = self.clip(rectangle=(4, 22, 14, 23), scale=(27, 47))
+        sprites['player_right'] = []
+        sprites['player_right'].append(self.clip(rectangle=(4, 22, 14, 23), scale=(27, 47)))
+        sprites['player_right'].append(self.clip(rectangle=(19, 22, 14, 23), scale=(27, 43)))
+        sprites['player_right'].append(self.clip(rectangle=(35, 21, 13, 19), scale=(27, 43)))
+        sprites['player_right'].append(self.clip(rectangle=(53, 21, 16, 19), scale=(27, 43)))
+        sprites['player_left'] = [pygame.transform.flip(x, True, False)
+                                  for x in sprites['player_right']]
+        sprites['green_full_brick'] = self.clip(rectangle=(6, 160, 9, 8), scale=(16, 13))
         return sprites
 
 
@@ -124,12 +134,36 @@ class MainGame:
         print('main_game()')
         screen.fill((0, 0, 0))
         screen.blit(images['level'], (0, -1190))
-        screen.blit(sprites['player'], (256, 415))
+        screen.blit(sprites['player_right'][0], (256, 415))
+        screen.blit(sprites['green_full_brick'], (128, 360))
         sounds['background'].play(-1)
         pygame.display.update()
 
     def jump(self):
         sounds['jump'].play()
+
+
+class Movement:
+    def __init__(self, pos, speed):
+        self.x, self.y = pos
+        self.speed = speed
+
+    def left(self, sprite):
+        self.x -= self.speed
+        self.draw(sprite)
+
+    def right(self, sprite):
+        self.x += self.speed
+        self.draw(sprite)
+
+    def draw(self, sprite):
+        self.erase()
+        screen.blit(sprite, (self.x, self.y))
+        pygame.display.update()
+
+    def erase(self):
+        #pygame.draw.rect(screen, (0,0,0), (self.x, self.y, 27, 39))
+        pygame.draw.rect(screen, (0,0,0), (self.x-4, self.y, 35, 39))
 
 
 loader = LoadAssets(assets='assets')
@@ -139,21 +173,38 @@ sounds = loader.sounds()
 sheet = SpriteSheet('assets/sprites/SpriteSheetTweaked.png')
 sprites = sheet.make_clips()
 
+move = Movement(pos=PLAYER_POS, speed=PLAYER_SPEED)
+
 MainMenu()
 MainGame()
 
-is_in_game = False
 done = False
-
+r_sprite = 0
+l_sprite = 0
 while not done:
+    keys = pygame.key.get_pressed()
+    if keys[K_RIGHT]:
+        r_sprite += 1
+        if r_sprite > 3:
+            r_sprite = 0
+        print('move.right()')
+        move.right(sprites['player_right'][r_sprite])
+        pygame.time.wait(60)
+        move.draw(sprites['player_right'][0])
+    elif keys[K_LEFT]:
+        l_sprite += 1
+        if l_sprite > 3:
+            l_sprite = 0
+        print('move.left()')
+        move.left(sprites['player_left'][l_sprite])
+        pygame.time.wait(60)
+        move.draw(sprites['player_left'][0])
+
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == KEYDOWN and event.key == K_RETURN and not is_in_game:
-            main_menu()
-            is_in_game = True
-        if event.type == KEYDOWN and event.key == K_x and is_in_game:
-            jump()
+        print(event)
+        if event.type == 2 and event.key == 120:
+            pass
     # run the window with max 60 fps
-    pygame.time.Clock().tick(60)
-    pygame.display.update()
+    #pygame.display.update()
+    #pygame.time.Clock().tick(60)
